8aa47fe13db16634ba8865ab05b041cf684e421d
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-05-26 18:06:38 -0600

    express basic route

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
 
 app.listen(3000, () => console.log("Server running on port 3000"));
 
-// Ruta con "Hello world"
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
