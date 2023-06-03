139201e39ff56fe93d12bba13768daf04ad5e853
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-03 17:42:45 -0600

    get all products

--- server/controller/products.controller.ts
+++ server/controller/products.controller.ts
@@ -1,7 +1,32 @@
 import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const getProducts = (req: Request, res: Response) => {
-  res.send(`Server response: products`);
+  const products = prisma.product
+    .findMany({
+      select: {
+        ProductId: true,
+        ProductName: true,
+        Description: true,
+        Price: true,
+        Quantity: true,
+        CategoryId: true,
+        Status: true,
+      },
+    })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.json(error);
+    });
+
+  if (!products) {
+    return res.status(404).json({ message: "No products found" });
+  }
 };
 
 export const getProduct = (req: Request, res: Response) => {
