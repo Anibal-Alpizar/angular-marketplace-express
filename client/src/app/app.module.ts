306dae665b93a673ecdeda9e9deb6868dc9bfcab
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-21 22:59:07 -0600

    first initial modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,17 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
+import { ShareModule } from './share/share.module';
+import { HomeModule } from './home/home.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
+
+    // my modules
+    CoreModule,
+    ShareModule,
+    HomeModule,
+
+    BrowserAnimationsModule,
+
+    // my routing
     AppRoutingModule,
-    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
