9ffb296551c88e86c32d3e223a77fe97b0a0d3a6
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-22 19:07:54 -0600

    toast notification

--- client/src/app/share/notification.service.ts
+++ client/src/app/share/notification.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { IndividualConfig, ToastrService } from 'ngx-toastr';
+
+export enum TypeMessage {
+  Success,
+  Error,
+  Info,
+  Warning,
+  Question,
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificationService {
-
-  constructor() { }
+  options: IndividualConfig;
+  constructor(private toastr: ToastrService) {
+    this.options = this.toastr.toastrConfig;
+    this.options.enableHtml = true;
+    this.options.positionClass = 'toast-top-right';
+    this.options.disableTimeOut = false;
+    this.options.closeButton = true;
+  }
+  public messageSuccess(
+    title: string,
+    message: string,
+    typeMessage: TypeMessage
+  ) {
+    this.toastr.show(
+      message,
+      title,
+      this.options,
+      'toast-' + TypeMessage[typeMessage]
+    );
+  }
 }
