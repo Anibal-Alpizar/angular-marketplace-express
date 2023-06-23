9a6fa6f258db17f887ea15e7abb3d8c1642e7889
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-22 19:08:30 -0600

    crud service api

--- client/src/app/share/generic.service.ts
+++ client/src/app/share/generic.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GenericService {
+  urlAPI: string = environment.apiURL;
+  currentUser: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  list(endPoint: string): Observable<any> {
+    return this.http.get<any>(this.urlAPI + endPoint);
+  }
+
+  get(endPoint: string, filter: any): Observable<any | any[]> {
+    return this.http.get<any | any[]>(this.urlAPI + endPoint + `/${filter}`);
+  }
 }
