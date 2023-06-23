5707a01b973a645ac426b85a2a8348e2d403958a
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-22 22:46:47 -0600

    fixed dark mode

--- client/src/app/core/header/header.component.ts
+++ client/src/app/core/header/header.component.ts
@@ -7,7 +7,8 @@ import { DarkModeService } from 'src/app/share/dark-mode.service';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  constructor(private darkModeService: DarkModeService) {}
+  constructor(private darkModeService: DarkModeService) {
+  }
 
   toggleDarkMode() {
     this.darkModeService.toggleDarkMode();
