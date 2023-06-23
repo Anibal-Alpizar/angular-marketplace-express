1d7b3c16c22b66f52160d8a7394ce07ba718eac8
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-22 19:08:41 -0600

    change it

--- client/src/app/product/product-by-vendor/product-by-vendor.component.ts
+++ client/src/app/product/product-by-vendor/product-by-vendor.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-product-by-vendor',
+//   templateUrl: './product-by-vendor.component.html',
+//   styleUrls: ['./product-by-vendor.component.css']
+// })
+// export class ProductByVendorComponent {
+
+// }
+
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product-by-vendor',
   templateUrl: './product-by-vendor.component.html',
-  styleUrls: ['./product-by-vendor.component.css']
+  styleUrls: ['./product-by-vendor.component.css'],
 })
-export class ProductByVendorComponent {
+export class ProductByVendorComponent implements OnInit {
+  products: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getProductsByVendor();
+  }
+
+  getProductsByVendor(): void {
+    this.http.get<any[]>('http://localhost:3000/products').subscribe(
+      (response) => {
+        this.products = response;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
