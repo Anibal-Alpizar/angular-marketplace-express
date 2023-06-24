03734d72739233545718c7e985d679e23b8989b4
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-23 22:46:52 -0600

    by user

--- client/src/app/product/product-routing.module.ts
+++ client/src/app/product/product-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductByVendorComponent } from './product-by-vendor/product-by-vendor.component';
+import { ProductByUserComponent } from './product-by-user/product-by-user.component';
 
 const routes: Routes = [
   {
     path: 'product-by-vendor',
     component: ProductByVendorComponent,
+  },
+  {
+    path: 'products/all',
+    component: ProductByUserComponent,
   },
 ];
 
