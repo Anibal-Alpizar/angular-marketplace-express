d6c3d0ef7d8d3816c8b17ddf13f1949084a5ebd1
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-25 22:52:19 -0600

    logic product detail

--- client/src/app/product/product-detail/product-detail.component.ts
+++ client/src/app/product/product-detail/product-detail.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject, pipe, takeUntil } from 'rxjs';
+import { GenericService } from 'src/app/share/generic.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent {
+  data: any;
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
+  constructor(private gService: GenericService, private route: ActivatedRoute) {
+    let id = this.route.snapshot.paramMap.get('id');
+    if (!isNaN(Number(id))) {
+      this.getProduct(Number(id));
+    }
+  }
+  getProduct(id: any) {
+    this.gService
+      .getProductDetails(id)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: any) => {
+        console.log(data);
+        this.data = data;
+      });
+  }
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 }
