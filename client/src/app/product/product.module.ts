3a56b29cf5f8774446be1fd0c677309555f01a77
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-25 22:51:54 -0600

    component added

--- client/src/app/product/product.module.ts
+++ client/src/app/product/product.module.ts
@@ -5,12 +5,14 @@ import { ProductRoutingModule } from './product-routing.module';
 import { TableComponent } from '../components/table/table.component';
 import { CardComponent } from '../components/card/card.component';
 import { ProductByUserComponent } from './product-by-user/product-by-user.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     ProductByVendorComponent,
     ProductByUserComponent,
+    ProductDetailComponent,
     TableComponent,
     CardComponent,
   ],
