abb52359ba4a5273cbe540d5d07f67b35470aad9
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-07-03 22:51:44 -0600

    component object

--- client/src/app/order/order.module.ts
+++ client/src/app/order/order.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderByUserComponent } from './order-by-user/order-by-user.component';
 import { OrderRoutingModule } from './order-routing.module';
+import { TableOrdersComponent } from '../components/table-orders/table-orders.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [OrderByUserComponent],
-  imports: [CommonModule, OrderRoutingModule],
+  declarations: [OrderByUserComponent, TableOrdersComponent],
+  imports: [CommonModule, OrderRoutingModule, FormsModule],
 })
 export class OrderModule {}
