0fff2fff5844bd39314e79d9b22528d849f60252
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-07-05 19:29:28 -0600

    orden details done

--- client/src/app/order/order.module.ts
+++ client/src/app/order/order.module.ts
@@ -4,9 +4,10 @@ import { OrderByUserComponent } from './order-by-user/order-by-user.component';
 import { OrderRoutingModule } from './order-routing.module';
 import { TableOrdersComponent } from '../components/table-orders/table-orders.component';
 import { FormsModule } from '@angular/forms';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
 
 @NgModule({
-  declarations: [OrderByUserComponent, TableOrdersComponent],
+  declarations: [OrderByUserComponent, TableOrdersComponent, OrderDetailComponent],
   imports: [CommonModule, OrderRoutingModule, FormsModule],
 })
 export class OrderModule {}
