20ca985db4e7ae36b16fc6458400c24139010cb2
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-07-09 19:46:57 -0600

    updated: images to array

--- server/prisma/seeds/photos.ts
+++ server/prisma/seeds/photos.ts
@@ -24,6 +24,39 @@ export const photos: Photo[] = [
   {
     PhotoId: 5,
     ProductId: 1,
-    PhotoURL: "https://m.media-amazon.com/images/I/6139eFMd4zL._AC_SX522_.jpg",
+    PhotoURL: "https://m.media-amazon.com/images/I/61GJ7bC8EzS._AC_SX569_.jpg",
+  },
+  {
+    PhotoId: 6,
+    ProductId: 1,
+    PhotoURL: "https://m.media-amazon.com/images/I/517HP7eS+NS._AC_SX569_.jpg",
+  },
+  {
+    PhotoId: 7,
+    ProductId: 1,
+    PhotoURL: "https://m.media-amazon.com/images/I/71Y5vOITojS._AC_SX569_.jpg",
+  },
+
+  {
+    PhotoId: 8,
+    ProductId: 4,
+    PhotoURL: "https://m.media-amazon.com/images/I/511GCg6biUL._AC_SX522_.jpg",
+  },
+
+  {
+    PhotoId: 9,
+    ProductId: 4,
+    PhotoURL: "https://m.media-amazon.com/images/I/717Jj9V2nhL._AC_SY879_.jpg",
+  },
+  {
+    PhotoId: 10,
+    ProductId: 4,
+    PhotoURL: "https://m.media-amazon.com/images/I/61RJOYKq+CL._AC_SY879_.jpg",
+  },
+
+  {
+    PhotoId: 11,
+    ProductId: 3,
+    PhotoURL: "https://m.media-amazon.com/images/I/51XBGD1GtML._AC_SX679_.jpg",
   },
 ];
