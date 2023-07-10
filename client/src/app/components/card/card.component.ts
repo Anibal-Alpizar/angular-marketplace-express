487f26e890814c90eea072a5e46ec1176a3b795b
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-07-09 19:15:35 -0600

    products images from the backend

--- client/src/app/components/card/card.component.ts
+++ client/src/app/components/card/card.component.ts
@@ -7,5 +7,9 @@ import { Product } from 'src/app/product/interfaces/product';
   styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
+  getFirstImageUrl(product: Product): string | undefined {
+    return product?.Photos?.[0]?.PhotoURL;
+  }
+
   @Input() filterProducts: Product[] = [];
 }
