2af7f1847edf0b2444e514ce78cadd33ea92a07f
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-07-28 22:58:03 -0600

    !show navbar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MarketPlace';
+  isLoginRoute = false;
+
+constructor(private router: Router, private route: ActivatedRoute) {}
+
+ ngOnInit() {
+  this.router.events.subscribe((event) => {
+    if (event instanceof NavigationEnd) {
+      this.isLoginRoute = this.route.snapshot.firstChild?.routeConfig?.path === 'login'; 
+    }
+  });
+}
+
 }
