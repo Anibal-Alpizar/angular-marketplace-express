839e5607a0fe6953bb7a5eb3fdef92ed68b6b919
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-07-28 22:56:19 -0600

    delete all routes from routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,23 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
-import { ProductByVendorComponent } from './product/product-by-vendor/product-by-vendor.component';
-import { ProductByUserComponent } from './product/product-by-user/product-by-user.component';
-import { ProductDetailComponent } from './product/product-detail/product-detail.component';
-import { OrderByUserComponent } from './order/order-by-user/order-by-user.component';
-import { OrderDetailComponent } from './order/order-detail/order-detail.component';
-import { OrderByVendorComponent } from './order/order-by-vendor/order-by-vendor.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
-  { path: 'product-by-vendor', component: ProductByVendorComponent },
-  { path: 'products/all', component: ProductByUserComponent },
-  { path: 'product/:id', component: ProductDetailComponent },
-  { path: 'orders', component: OrderByUserComponent },
-  { path: 'orders/:id', component: OrderDetailComponent },
-  { path: 'orders-by-vendor', component: OrderByVendorComponent },
+
+  { path: 'login', outlet: 'userOutlet', component: UserLoginComponent },
 ];
 
 @NgModule({
