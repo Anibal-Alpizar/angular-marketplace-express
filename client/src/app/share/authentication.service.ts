e7d5c00b5e339cce6c86e5d0c9db8c4f34134bc0
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-07-29 20:48:36 -0600

    auth services

---
+++ client/src/app/share/authentication.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { CartService } from './cart.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationService {
+  ServerUrl = environment.apiURL;
+  private currentUserSubject: BehaviorSubject<any>;
+  public currentUser: Observable<any>;
+  private authenticated = new BehaviorSubject<boolean>(false);
+
+  constructor(private http: HttpClient, private cartService: CartService) {
+    this.currentUserSubject = new BehaviorSubject<any>(
+      JSON.parse(localStorage.getItem('currentUser') || '{}')
+    );
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+  public get currentUserValue(): any {
+    return this.currentUserSubject.value;
+  }
+
+  get isAuthenticated() {
+    if (this.authenticated.value != null) {
+      this.authenticated.next(true);
+    } else {
+      this.authenticated.next(false);
+    }
+    return this.authenticated.asObservable();
+  }
+
+  register(user: any): Observable<any> {
+    return this.http.post<any>(this.ServerUrl + 'register', user);
+  }
+}
