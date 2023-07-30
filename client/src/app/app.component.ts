b32f1d3f66c57be31b07385dc885e4d9c0282e5a
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-07-29 19:42:54 -0600

    login and register

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,14 +11,14 @@ export class AppComponent implements OnInit {
   title = 'MarketPlace';
   isLoginRoute = false;
 
-constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
- ngOnInit() {
-  this.router.events.subscribe((event) => {
-    if (event instanceof NavigationEnd) {
-      this.isLoginRoute = this.route.snapshot.firstChild?.routeConfig?.path === 'login'; 
-    }
-  });
-}
-
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isLoginRoute =
+          this.route.snapshot.firstChild?.routeConfig?.path === 'register';
+      }
+    });
+  }
 }
