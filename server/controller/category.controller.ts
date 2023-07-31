e5d49d28048fadf69ee6c995f196dbc96c137407
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-07-31 11:30:41 -0600

    get all categories

---
+++ server/controller/category.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const getCategories = async (req: Request, res: Response) => {
+  try {
+    const categories = await prisma.category.findMany();
+
+    res.json(categories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
