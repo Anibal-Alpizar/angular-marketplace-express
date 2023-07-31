171c911ff6937061c42831a6caa899971911e807
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-07-31 11:30:13 -0600

    path for images

--- server/index.ts
+++ server/index.ts
@@ -4,10 +4,12 @@ import { PORT } from "./config";
 import indexRoutes from "./routes/index.routes";
 import productsRoutes from "./routes/products.routes";
 import purchaseItemRoutes from "./routes/purchaseItem.routes";
-import questions from './routes/questions.routes';
-import answer from './routes/answer.routes';
+import questions from "./routes/questions.routes";
+import answer from "./routes/answer.routes";
 import userRoutes from "./routes/user.routes";
-import fileUpload from 'express-fileupload'
+import categoriesRoutes from "./routes/category.routes";
+import fileUpload from "express-fileupload";
+import path from "path";
 
 const app = express();
 
@@ -19,6 +21,9 @@ app.use(
 
 app.use(express.json());
 app.use(fileUpload());
+
+app.use("/uploads", express.static(path.resolve("uploads")));
+
 // Routes
 app.use(indexRoutes);
 app.use(productsRoutes);
@@ -26,6 +31,7 @@ app.use(purchaseItemRoutes);
 app.use(userRoutes);
 app.use(questions);
 app.use(answer);
+app.use(categoriesRoutes);
 
 app.listen(PORT || 3000, () => {
   console.log(`Server is running on port ${PORT}`);
