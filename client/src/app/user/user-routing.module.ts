f8cedfef15baabb4865112375d97be91d27ff10f
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-07-31 18:34:45 -0600

    about me route

--- client/src/app/user/user-routing.module.ts
+++ client/src/app/user/user-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserRegisterComponent } from './user-register/user-register.component';
+import { UserAboutComponent } from './user-about/user-about.component';
 
 const routes: Routes = [
   { path: 'login', component: UserLoginComponent },
   { path: 'register', component: UserRegisterComponent },
+  { path: 'about', component: UserAboutComponent },
 ];
 
 @NgModule({
