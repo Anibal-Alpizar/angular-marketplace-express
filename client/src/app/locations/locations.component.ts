7fba25bfe919d7a9edbaf3db4082b7c0dd9ffd8b
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-08-11 19:25:34 -0600

    api done

--- client/src/app/locations/locations.component.ts
+++ client/src/app/locations/locations.component.ts
@@ -8,9 +8,11 @@ import { LocationService } from 'src/app/share/locations.service';
 })
 export class LocationsComponent implements OnInit {
   provinces: string[] = [];
-  selectedProvince: string = '';
+  districts: string[] = [];
   cantons: any = {};
   selectedCanton: string = '';
+  selectedDistrict: string = '';
+  selectedProvince: string = '';
 
   constructor(private lService: LocationService) {}
 
@@ -19,7 +21,36 @@ export class LocationsComponent implements OnInit {
   }
 
   onCantonSelected(event: any): void {
-    this.selectedCanton = event.target.value;
+    const selectedCantonName = event.target.value;
+
+    if (selectedCantonName) {
+      const selectedCantonNumber = Object.keys(
+        this.cantons[this.selectedProvince]
+      ).find(
+        (key) => this.cantons[this.selectedProvince][key] === selectedCantonName
+      );
+
+      if (selectedCantonNumber) {
+        console.log(`Selected Canton Number: ${selectedCantonNumber}`);
+        const provinceToDistrictMapping = {
+          'San José': '1',
+          Alajuela: '2',
+          Cartago: '3',
+          Heredia: '4',
+          Guanacaste: '5',
+          Puntarenas: '6',
+          Limón: '7',
+        };
+
+        const provinceNumber =
+          provinceToDistrictMapping[
+            this.selectedProvince as keyof typeof provinceToDistrictMapping
+          ];
+        this.loadDistricts(provinceNumber, selectedCantonNumber);
+      } else {
+        console.log('Canton number not found.');
+      }
+    }
   }
 
   loadProvinces(): void {
@@ -34,6 +65,26 @@ export class LocationsComponent implements OnInit {
       });
   }
 
+  loadDistricts(provinceNumber: string, cantonNumber: string): void {
+    this.lService
+      .getDistritos(provinceNumber, cantonNumber)
+      .then((data: any) => {
+        this.districts = Object.values(data);
+        console.log(
+          `Districts loaded for province ${provinceNumber}, canton ${cantonNumber}:`,
+          this.districts
+        );
+      })
+      .catch((error) => {
+        console.error('Error loading districts:', error);
+        throw error;
+      });
+  }
+
+  onDistrictSelected(event: any): void {
+    this.selectedDistrict = event.target.value;
+  }
+
   getCantonsForSelectedProvince(): string[] {
     if (this.selectedProvince) {
       return this.cantons[this.selectedProvince] || [];
@@ -45,7 +96,7 @@ export class LocationsComponent implements OnInit {
     this.lService
       .getCantons(provinceNumber)
       .then((data: any) => {
-        this.cantons[this.selectedProvince] = Object.values(data); 
+        this.cantons[this.selectedProvince] = Object.values(data);
         console.log(
           `Cantons loaded for province ${this.selectedProvince}:`,
           this.cantons[this.selectedProvince]
@@ -67,9 +118,9 @@ export class LocationsComponent implements OnInit {
         const provinceNumber = Object.keys(data).find(
           (key) => data[key] === selectedProvinceName
         );
+
         if (provinceNumber) {
           console.log(`Selected Province Number: ${provinceNumber}`);
-
           this.loadCantons(provinceNumber); 
         } else {
           console.log('Province number not found.');
