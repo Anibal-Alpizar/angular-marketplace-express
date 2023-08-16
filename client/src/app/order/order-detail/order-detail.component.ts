c019385e5bf9245409663e95048153c1c16fcb6d
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-08-15 21:12:53 -0600

    payment method added in the detils

--- client/src/app/order/order-detail/order-detail.component.ts
+++ client/src/app/order/order-detail/order-detail.component.ts
@@ -1,19 +1,24 @@
-import { Component, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject, takeUntil } from 'rxjs';
+import { Observable, Subject, takeUntil } from 'rxjs';
 import { OrdersService } from 'src/app/share/orders.service';
 import * as moment from 'moment';
+import { PaymentMethod } from 'src/app/interfaces/payment.interface';
+import { PaymentService } from 'src/app/share/payment.service';
 
 @Component({
   selector: 'app-order-detail',
   templateUrl: './order-detail.component.html',
   styleUrls: ['./order-detail.component.css'],
 })
-export class OrderDetailComponent implements AfterViewInit, OnDestroy {
+export class OrderDetailComponent implements AfterViewInit, OnDestroy, OnInit {
   data: any;
   destroy$: Subject<boolean> = new Subject<boolean>();
   totalPrice: number = 0;
   totalTaxes: number = 0;
+  savedPaymentMethods$: Observable<PaymentMethod[]> | undefined;
+  savedPaymentMethods: PaymentMethod[] = [];
+  selectedPaymentMethod: PaymentMethod | undefined;
 
   formatDate(dateString: string): string {
     if (!dateString) {
@@ -26,15 +31,47 @@ export class OrderDetailComponent implements AfterViewInit, OnDestroy {
     return formattedDate;
   }
 
-  constructor(private gService: OrdersService, private route: ActivatedRoute) {}
+  constructor(
+    private gService: OrdersService,
+    private route: ActivatedRoute,
+    private paymentService: PaymentService
+  ) {}
 
   ngAfterViewInit() {
     let id = this.route.snapshot.paramMap.get('id');
     if (!isNaN(Number(id))) {
       this.getOrder(Number(id));
     }
+    const currentUser = JSON.parse(localStorage.getItem('currentUser') || '{}');
+    const userId = currentUser?.user?.UserId;
+    console.log('userId:', userId);
   }
 
+  ngOnInit(): void {
+    this.getPaymentMethodsForCurrentUser();
+  }
+
+  getPaymentMethodsForCurrentUser() {
+    const currentUserJson = localStorage.getItem('currentUser');
+    if (currentUserJson) {
+      const currentUser = JSON.parse(currentUserJson);
+      if (currentUser.user && currentUser.user.UserId) {
+        const userId = currentUser.user.UserId;
+        this.savedPaymentMethods$ =
+          this.paymentService.getPaymentMethodsByUserId(userId);
+
+        this.paymentService.getPaymentMethodsByUserId(userId).subscribe(
+          (response: PaymentMethod[]) => {
+            this.savedPaymentMethods = response;
+            console.log('savedPaymentMethods:', this.savedPaymentMethods);
+          },
+          (error) => {
+            console.error('Error getting payment methods:', error);
+          }
+        );
+      }
+    }
+  }
   decreaseQuantity(index: number) {
     if (this.data[index].Quantity > 1) {
       this.data[index].Quantity--;
