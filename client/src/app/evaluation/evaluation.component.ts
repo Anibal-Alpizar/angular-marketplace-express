42cc9ad3700606681841ea073db6e7e766acc93f
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-08-19 23:26:14 -0600

    create evaluation done

--- client/src/app/evaluation/evaluation.component.ts
+++ client/src/app/evaluation/evaluation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NotificationService } from '../share/notification.service';
+import { EvaluationService } from '../share/evaluation.service';
 
 @Component({
   selector: 'app-evaluation',
@@ -7,39 +8,38 @@ import { NotificationService } from '../share/notification.service';
   styleUrls: ['./evaluation.component.css'],
 })
 export class EvaluationComponent implements OnInit {
-  numeroSeleccionado: number | null = null;
+  rating: number = 0;
   comentario: string = '';
   userId: number = 0;
   purchaseId: number = 0;
 
-  constructor(private notification: NotificationService) {}
+  constructor(
+    private notification: NotificationService,
+    private evaluationService: EvaluationService
+  ) {}
 
   ngOnInit(): void {
-    this.getUserId();
     this.loadPurchaseId();
   }
 
   handleRatingClick(rating: number) {
-    this.numeroSeleccionado = rating;
+    this.rating = rating;
   }
 
   send() {
-    if (this.numeroSeleccionado !== null) {
-      console.log('Número seleccionado:', this.numeroSeleccionado);
-      console.log('Comentario:', this.comentario);
-    } else {
-      console.log('No se ha seleccionado ningún número.');
-    }
-  }
-
-  getUserId() {
     const currentUserJson = localStorage.getItem('currentUser');
     if (currentUserJson) {
       const currentUser = JSON.parse(currentUserJson);
-      if (currentUser.user && currentUser.user.UserId) {
-        const userId = currentUser.user.UserId;
-        console.log('userId:', userId);
-      }
+      const userId = currentUser.user.UserId;
+
+      this.evaluationService
+        .sendEvaluation(userId, this.rating, this.comentario, this.purchaseId)
+        .subscribe((response) => {
+          console.log('response:', response);
+          this.notification.showSuccess('Gracias por tu evaluación.');
+        });
+    } else {
+      this.notification.showError('No se ha encontrado el usuario.');
     }
   }
 
--- client/src/app/share/evaluation.service.ts
+++ client/src/app/share/evaluation.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EvaluationService {
+  urlAPI: string = environment.apiURL;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  private handleError(error: any) {
+    console.error('An error occurred:', error);
+    return throwError('Something went wrong; please try again later.');
+  }
+
+  sendEvaluation(
+    userId: number,
+    rating: number,
+    comment: string,
+    purchaseId: number
+  ) {
+    const url = `${this.urlAPI}/createEvaluation`;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    const evaluationData = {
+      userId: userId,
+      rating: rating,
+      comment: comment,
+      PurchaseId: purchaseId,
+    };
+
+    return this.http
+      .post(url, evaluationData, { headers })
+      .pipe(catchError(this.handleError));
+  }
 }
